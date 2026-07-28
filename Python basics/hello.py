2ed8f8acc1675a412ffe8c7cf1caba4d48d9a20c
Pavani-cs <pavaniganjai05@gmail.com>
2026-07-28 12:22:37 -0400

    First commit

---
+++ Python basics/hello.py
@@ -0,0 +1,8 @@
+print("Hello world!")              # It Display output on the screen
+print("Python!")
+print("Welcome to Python")
+print("Happy coding")
+print("BASICS")
+# this program prints simple sentence
+print("Python is easy to learn")
+
