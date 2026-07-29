36c0343bafc270a409d5cae5ffaccc3290af8e55
Pavani-cs <pavaniganjai05@gmail.com>
2026-07-29 12:03:48 -0400

    variables.py

---
+++ Python basics/variables.py
@@ -0,0 +1,27 @@
+# A variable is a name used to store a value
+name="python"
+age=20
+print(name)
+print(age)
+print(name,age)
+print(age,name)
+
+a = 10; b = 90; print(a,b)
+
+NAME="P"
+print(NAME)
+
+_Num=10; print(_Num)
+
+x=5
+x="hello"
+print(x)
+#variables are case sentive 
+a=7
+A="key"
+print(a)
+print(A)
+
+a1=80
+a2=40
+print(a1,a2)
