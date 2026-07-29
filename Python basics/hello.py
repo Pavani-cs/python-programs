116bf83f7633c399ec21c3d53aa5d12f02611c94
Pavani-cs <pavaniganjai05@gmail.com>
2026-07-29 11:19:41 -0400

    hello.py

--- Python basics/hello.py
+++ Python basics/hello.py
@@ -1,10 +1,19 @@
 print("Hello world!")              # It Display output on the screen
-print("Python!")
+print("Python!")                   #Double quotes
 print("Welcome to Python")
-print("Happy coding")
+print("Happy coding")                   
 print("BASICS")
 # this program prints simple sentence
 print("Python is easy to learn")
 # when  a write multiple statements on one line by separating them with ;
 print("pavani");print("shivani");print("ruthvik")
-print('This will also work')
+print('This will also work')                        #Single quotes
+#print multiple words on the same line,use end parameter
+print("computer",end=" ") 
+print("science",end=" ")
+print("student.")
+# Numbers
+print(8)
+print(3+5)
+#combine text and numbers in one output by separating them with a comma
+print("Iam",40,"years old.")
