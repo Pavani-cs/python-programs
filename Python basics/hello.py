4cacd5568f1541f6fe639699ec0459f0289f5c32
Pavani-cs <pavaniganjai05@gmail.com>
2026-07-29 10:42:05 -0400

    hello.py

--- Python basics/hello.py
+++ Python basics/hello.py
@@ -5,4 +5,5 @@ print("Happy coding")
 print("BASICS")
 # this program prints simple sentence
 print("Python is easy to learn")
-
+#when  a write multiple statement on one line by separating them with ;
+print("pavani");print("shivani");print("ruthvik")
