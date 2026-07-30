7267fdd1ac71c8e36e39c33e539370dea7ecfd59
Pavani-cs <pavaniganjai05@gmail.com>
2026-07-30 06:33:59 -0400

    variables.py

--- Python basics/variables.py
+++ Python basics/variables.py
@@ -16,7 +16,8 @@ _Num=10; print(_Num)
 x=5
 x="hello"
 print(x)
-#variables are case sentive 
+
+#variables are case sensitive
 a=7
 A="key"
 print(a)
@@ -33,5 +34,116 @@ print("Roll No:",roll ,"Name:",name)
 
 year=2026
 print("Welcome to the year", year)
+
 Apples=10
 print("I have",Apples,"apples.")
+
+# Many values to multiple variables
+x,y,z="orange","apple","cherry"
+print(x)
+print(y)
+print(z)
+
+lang1,lang2,lang3= "Python", "C", "Java"
+print(lang1,lang2,lang3)
+print(lang1 + " " + lang2 + " " + lang3)    # also use the + operator to print multiple variables
+
+x="Pen "
+y="Pencil "
+z="Book"
+print(x+y+z)
+
+#one value to multiple variables
+x=y=z="Laptop"
+print(x)
+print(y)
+print(z)
+
+# local variables
+def show_name():
+    name="Post"
+    print(name)
+show_name()
+
+def add():
+    a=10
+    b=5
+    c=a+b
+    print("The sum is:",c)
+add()
+
+def value():
+    num=50
+    print(num)
+value()
+num=100
+print(num)
+
+def area_of_the_rectangle():
+    length=8
+    width=5
+    print("Area of rectangle:",length*width)
+area_of_the_rectangle()
+
+def language():
+    first='Python'
+    second='Programming'
+    print(first + " " + second)
+    print(first,second)
+language()
+
+# Global variables
+x=200
+def number():
+    print("x:",x)
+number()
+
+x="awesome"
+def myfun():
+    print("Python is",x)
+    print("Python is "+x)
+myfun()
+
+x="awesome"
+def myfun():
+    x="fantastic"
+    print(x)
+myfun()
+print(x)
+
+def myfun():                      # Global keyword
+    global x
+    x="python"
+myfun()
+print("Programming language:"+ " "+ x)
+
+x="cat"
+def myfun():
+    global x
+    x="fan"
+myfun()
+print(x)
+
+x=101
+def mainFunction():
+    global x
+    print(x)
+    x="Welcome to Hyderabad"
+    print(x)
+mainFunction()
+print(x)
+
+def mainfunction():
+    global num
+    num=100
+    print(num)
+mainfunction()
+print(num)
+
+# Take an integer input from the user
+a=int(input("enter a:"))
+print(a)
+
+# Take a string (text) input from the user
+a=input("Enter a:")
+print(a)
