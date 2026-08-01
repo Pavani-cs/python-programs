6d5813f5083b05d0621a396165e848d3659b87a6
Pavani-cs <pavaniganjai05@gmail.com>
2026-08-01 11:27:37 -0400

    data types.py

---
+++ Python basics/data types.py
@@ -0,0 +1,61 @@
+# Getting  the data type
+x = 1
+print(type(x))
+x = "World"
+print(type(x))
+x = 1.3
+print(type(x))
+x = 1j
+print(type(x))
+x = ["apple", "mango"]
+print(type(x))
+x = ("nirmal", "armoor")
+print(type(x))
+x = range(6)
+print(type(x))
+x = {"name" : "john", "age" : 21}
+print(type(x))
+x = {"school", "bag" ,"basket"}
+print(type(x))
+x = frozenset( {"air" , "bug", "aim"})
+print(type(x))
+x = True
+print(type(x))
+x = b"Hello"
+print(type(x))
+x = bytearray(5)
+print(type(x))
+x = memoryview(bytes(5))
+print(type(x))
+x = None
+print(type(x))
+
+# setting the specific data type
+x = int(1)
+print(x)
+x = str("World")
+print(x)
+x = float(1.3)
+print(x)
+x =complex(1j)
+print(x)
+x = list(("apple", "mango"))
+print(x)
+x = tuple(("nirmal", "armoor"))
+print(x)
+x = range(6)
+print(x)
+x = dict(name = "john", age = 21)
+print(x)
+x = set(("school", "bag" ,"basket"))
+print(x)
+x = frozenset(("air" , "bug", "aim"))
+print(x)
+x = bool(5)
+print(x)
+x = bytes(5)
+print(x)
+x = bytearray(5)
+print(x)
+x = memoryview(bytes(5))
+print(x)
