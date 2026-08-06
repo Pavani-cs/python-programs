5a8b739711001c19773d4ded70698a367b270aa4
Pavani-cs <pavaniganjai05@gmail.com>
2026-08-06 12:59:05 -0400

    data types.py

--- Python basics/data types.py
+++ Python basics/data types.py
@@ -142,3 +142,10 @@ a = 10
 b = str(a)
 print(b, type (b))
 
+# Example
+num = input("Enter number:")
+print(num)
+name1 = input("Enter name:")
+print(name1)
+print("type of number",type(num))
+print("type of name",type(name1))
