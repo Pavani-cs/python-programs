82181a6c6012af3085dbe1d21c274b42108099fe
Pavani-cs <pavaniganjai05@gmail.com>
2026-08-12 12:34:46 -0400

    Comparison operators.py

---
+++ Python basics/Operators/Comparison operators.py
@@ -0,0 +1,38 @@
+x = 5
+y = 2
+print(x == y)
+
+x = 5
+y = 2
+print(x != y)
+
+x = 10
+y = 2
+print(x > y)
+
+x = 10
+y = 2
+print(x < y)
+
+x = 5
+y = 2
+print(x >= y)
+
+x = 5
+y = 2
+print(x <= y)
+
+# Examples
+x = 50
+y = 100
+print(x == y)
+print(x != y)
+print(x > y)
+print(x < y)
+print(x >= y)
+print(x <= y)
+
+# Chaining comparison operators
+x = 8
+print(1 < x < 10)
+print(1 < x and x < 10)
