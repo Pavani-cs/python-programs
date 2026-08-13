a35a0393ed18251263a305018f68b6de5071c6c2
Pavani-cs <pavaniganjai05@gmail.com>
2026-08-13 12:31:32 -0400

    Comparison operators.py

--- Python basics/Operators/Comparison operators.py
+++ Python basics/Operators/Comparison operators.py
@@ -37,6 +37,9 @@ x = 8
 print(1 < x < 10)
 print(1 < x and x < 10)
 
+age = 18
+print(18 <= age <= 60)
+
 # Examples
 x = 100
 y = 100
@@ -46,8 +49,19 @@ marks = 35
 print(marks >= 35)
 print(marks < 35)
 
+num = int(input("Enter num:"))
+print( 10 < num < 100)
+
+a = 12
+b = 20
+c = 50
+print(a < b < c)
+
+marks = int(input("Enter marks:"))
+print(60 <= marks <= 100)
+
 temperature = 40
-print(temperature < 30)
+print(temperature < 30)  
 
 num1 = int(input("Enter num1:"))
 num2 = int(input("enter num2:"))
