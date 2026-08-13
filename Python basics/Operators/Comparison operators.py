ac8a896196a8c2bd8369852812668a5d39f12d23
Pavani-cs <pavaniganjai05@gmail.com>
2026-08-13 12:11:28 -0400

    Comparison operators.py

--- Python basics/Operators/Comparison operators.py
+++ Python basics/Operators/Comparison operators.py
@@ -36,3 +36,19 @@ print(x <= y)
 x = 8
 print(1 < x < 10)
 print(1 < x and x < 10)
+
+# Examples
+x = 100
+y = 100
+print(x == y )
+
+marks = 35
+print(marks >= 35)
+print(marks < 35)
+
+temperature = 40
+print(temperature < 30)
+
+num1 = int(input("Enter num1:"))
+num2 = int(input("enter num2:"))
+print(num1 == num2 )
