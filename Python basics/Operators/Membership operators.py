cfe75f996fd635112f12506566568bbd369e2f38
Pavani-cs <pavaniganjai05@gmail.com>
2026-08-18 12:01:46 -0400

    Membership operators.py

---
+++ Python basics/Operators/Membership operators.py
@@ -0,0 +1,17 @@
+name = "Phone"
+print("P" in name)     #in means value is present.
+print("p" in name)
+print('z' in name)
+
+name = "Python"  
+print("P" not in name)   # not in means value is not present
+print("Z" not in name)
+
+numbers = [10,20,30,40]
+print(10 in numbers)
+print(50 in numbers)
+print(50 not in numbers, 20 not in numbers)
+
+x = [10]
+print(10 in x)
+print(10 not in x)
