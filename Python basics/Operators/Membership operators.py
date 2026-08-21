02f740c073752864ee631e878186dea1171c8c83
Pavani-cs <pavaniganjai05@gmail.com>
2026-08-21 10:40:44 -0400

    Membership operators.py

--- Python basics/Operators/Membership operators.py
+++ Python basics/Operators/Membership operators.py
@@ -15,3 +15,22 @@ print(50 not in numbers, 20 not in numbers)
 x = [10]
 print(10 in x)
 print(10 not in x)
+
+fruits = ["apple","banana"]
+print("banana" in fruits)
+print("pineapple" not in fruits)
+
+x = [1, 2, 3]
+print("1" in x)
+print("1" not in x)
+
+x = ["10", "20", "30"]
+print(10 in x)
+print(10 not in x)
+print("10" in x)
+
+subjects = ["C", "Python"]
+subject = input("Enter subject:")
+print(subject in subjects )
+
+  
